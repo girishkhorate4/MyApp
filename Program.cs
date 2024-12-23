1b1933a12bfb23eb5367161140e4d76a1eb39335
Girish Khorate <khorategirish09@gmail.com>
2024-12-23 14:07:21 +0530

    First commit in Development branch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // This is sample project
-
+// This is change related to new Development branch
 Console.WriteLine("Hello, World!");
